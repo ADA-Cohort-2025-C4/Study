51e90d2e89077a741b82f078b74ab0132ae9a9d1
Isla <Islajiin@gmail.com>
2025-07-09 20:43:48 +0900

    Initial Commit

---
+++ WhatSoundStudy/WhatSoundStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatSoundStudyApp.swift
+//  WhatSoundStudy
+//
+//  Created by yeonjin on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatSoundStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
